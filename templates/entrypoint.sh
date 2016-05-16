3cf55680b859626d416206e0915c84c91c49b9cc
Ikenna Okpala <me@ikennaokpala.com>
2016-05-17 00:37:45 +0100

    Waiting until backend is available and debugging settings

--- templates/entrypoint.sh
+++ templates/entrypoint.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 set -e
+set -x
 
 cp /etc/varnish/default.vcl.original /etc/varnish/default.vcl
 
 for name in BACKEND_PORT_5118_TCP_PORT BACKEND_PORT_5118_TCP_ADDR VARNISH_HOST
 do
-    eval value=\$$name
-    sed -i "s|\${${name}}|${value}|g" /etc/varnish/default.vcl
+  eval value=\$$name
+  sed -i "s|\${${name}}|${value}|g" /etc/varnish/default.vcl
 done
 
 # default="$(cat /etc/varnish/default.vcl.original)"
 # echo $(eval echo \"$default\") > /etc/varnish/default.vcl
 counter=0
-while ! nc -vz $BACKEND_PORT_5118_TCP_ADDR $BACKEND_PORT_5118_TCP_PORT; do
+until $(curl --output /dev/null --silent --head --fail http://$BACKEND_PORT_5118_TCP_ADDR:$BACKEND_PORT_5118_TCP_PORT); do
   counter=$((counter+1));
-  if [ $counter -eq 60 ]; then break; fi;
+  if [ $counter -eq 90 ]; then break; fi;
   sleep 1;
 done
 # Start varnish and log
